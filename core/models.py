04e1f2362332478f517621a10227bbec841190c6
Mwita Chacha <jonahhchacha@gmail.com>
2022-02-16 06:25:15 +0300

    Adding cloudinary and profile model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    image = models.CloudinaryField()
+    email =  models.CharField(max_length=60)
+    username = models.CharField(max_length=100,default='')
+    def __str__(self):
+        return self.user 
