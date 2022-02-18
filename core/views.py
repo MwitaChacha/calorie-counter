7ee13e6a92cfb6fc0837f54ff0730d96fce6964e
Mwita Chacha <jonahhchacha@gmail.com>
2022-02-18 12:24:05 +0300

    Creating search for calories functionality

--- core/views.py
+++ core/views.py
@@ -11,16 +11,31 @@ def index(request):
 
 
 def search(request):
-    url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
-    
-    querystring = {"query":"chips"}
-    
-    headers = {
-        'x-rapidapi-host': "calorieninjas.p.rapidapi.com",
-        'x-rapidapi-key': config("calorie_key")
+    context = None
+    if 'food' in request.GET:
+        food = request.GET.get('food')
+        url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
+        
+        querystring = {"query":food}
+        
+        headers = {
+            'x-rapidapi-host': "calorieninjas.p.rapidapi.com",
+            'x-rapidapi-key': config("calorie_key")
+            }
+        
+        response = requests.request("GET", url, headers=headers, params=querystring).json()
+        items=response['items']
+        sugar=items[0]['sugar_g']
+        fibre=items[0]['fiber_g']
+        serving=items[0]['serving_size_g']
+        sodium=items[0]['sodium_mg']
+        fat=items[0]['fat_total_g']
+        calories=items[0]['calories']
+        cholesterol=items[0]['cholesterol_mg']
+        protein=items[0]['protein_g']
+        carbs=items[0]['carbohydrates_total_g']
+        context = {
+            'items':items,'sugar':sugar,'fibre':fibre,'serving':serving,'sodium':sodium,'fat':fat,'calories':calories,'cholesterol':cholesterol,'protein':protein,'carbs':carbs
         }
-    
-    response = requests.request("GET", url, headers=headers, params=querystring).json()
-    
-    print(response)
-    return render(request, 'search.html')
+        print(items)
+    return render(request, 'search.html', context)
