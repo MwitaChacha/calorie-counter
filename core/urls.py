cb6dd85a4556c2fe5c8cad8074baaf06e0d0e886
Mwita Chacha <jonahhchacha@gmail.com>
2022-02-18 08:52:27 +0300

    Consuming calorie tracker api

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('search-for-food-calories/',views.search,name='search')
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-
-
+import requests
+from decouple import config
 # @login_required(login_url='/accounts/login/')
 def index(request):
     # if not request.user.is_authenticated:
     #     return redirect('/accounts/login/')
     return render(request, 'index.html')
+
+
+def search(request):
+    url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
+    
+    querystring = {"query":"chips"}
+    
+    headers = {
+        'x-rapidapi-host': "calorieninjas.p.rapidapi.com",
+        'x-rapidapi-key': config("calorie_key")
+        }
+    
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    
+    print(response)
+    return render(request, 'search.html')
